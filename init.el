c5ebe09f4440e2fa78c6c2bc24041799f8826401
Nicolas Brantut <nicolas.brantut@normalesup.org>
2019-06-01 21:23:18 +0100

    test with all require statemens in init.el

--- init.el
+++ init.el
@@ -1,2 +1,23 @@
+(require 'bind-key)
+(require 'cl)
+(require 'package)
+(require 'yasnippet)
+(require 'multiple-cursors)
+
+;; activate powerline
+(add-to-list 'load-path (concat user-emacs-directory "lisp/emacs-powerline"))
+(require 'powerline)
+
+
+;;Activate julia mode
+(add-to-list 'load-path (concat user-emacs-directory "lisp/julia-emacs"))
+(require 'julia-mode)
+(require 'julia-repl)
+
+;;Active ein
+(require 'ein)
+(require 'ein-notebook)
+(require 'ein-subpackages)
+
 ;; Load the config
 (org-babel-load-file (concat user-emacs-directory "config.org"))
